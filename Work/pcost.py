5c78e0aa107e15b7f06d218b2dd896e74857084d
Frank Peraza <fxperaza@gmail.com>
2025-12-05 13:46:53 -0700

    Update 1b

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,51 @@
 # pcost.py
 #
+import sys
+import csv
 # Exercise 1.27
+# fields on each line: name, total shares, share price
+# field offsets
+name = 0
+shares = 1
+price = 2
+
+def pcost(filename):
+    total_price = 0
+
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+
+    # read, discard the headers line
+    headers = next(rows)
+    # each remaining line should have individual stock data
+    #for line in f:
+    for row in rows:
+        #stock_fields = line.split(',')
+        try:
+            #nshares = int(stock_fields[shares])
+            nshares = int(row[shares])
+        except ValueError:
+            print("Couldn\'t parse", row)
+        
+        try:
+            #share_price = float(stock_fields[price])
+            share_price = float(row[price])
+        except ValueError:
+            print("Couldn\'t parse", row)
+        
+        total_price += nshares * share_price
+
+    f.close()
+    return total_price
+
+def main():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+
+    total_cost = pcost(filename)
+    print(f'Total cost was {total_cost}')
+
+if __name__ == '__main__':
+    main()
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,91 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import os
+
+def read_portfolio(filename):
+    '''Reads portfolio file'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            #holding = (row[0], int(row[1]), float(row[2]))
+            holding = {'name':row[0], 'shares':int(row[1]), 'price':float(row[2])}
+            portfolio.append(holding)
+    return portfolio
+
+def read_prices(filename):
+    """
+    Read file having lines containing 1) Stock Name 2) Stock Price and
+    store the stock data in a dictionary
+    """
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row) == 0:
+                continue
+            prices[row[0]] = float(row[1])
+    return prices
+
+def evaluate_portfolio(portfolio, prices):
+    unique_holdings = set()
+    net = 0.0
+    stocks = read_prices(prices)
+    holdings = read_portfolio(portfolio)
+
+    for holding in holdings:
+        stock_name = holding['name']
+        if stock_name not in unique_holdings:
+            unique_holdings.add(stock_name)
+
+        stock_initial_price = holding['price']
+        stock_shares = holding['shares']
+        if stock_name in stocks:
+            stock_current_price = stocks[stock_name]
+            delta = (stock_current_price - stock_initial_price) * stock_shares 
+            net += delta
+        print(f'stock:{stock_name} org price:{stock_initial_price} cur price:{stock_current_price}')
+
+    print(f'{len(unique_holdings)} individual stocks')
+    return net
+
+def make_report(portfolio, prices):
+    table_entries = []
+    stocks = read_prices(prices)
+    holdings = read_portfolio(portfolio)
+
+    for holding in holdings:
+        name = holding['name']
+        initial_price = holding['price']
+        shares = holding['shares']
+        if name in stocks:
+            current_price = stocks[name]
+            delta = current_price - initial_price
+            table_entries.append((name, shares, current_price, delta))
+
+    return table_entries
+
+file1 = 'Data/portfolio.csv'
+file2 = 'Data/prices.csv'
+
+def main():
+    #cwd = os.getcwd()
+    #print('Main with cwd:', cwd)
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    hs = f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}'
+    #seps = f'{'-'*len(headers[0]):>10s} {'-'*len(headers[1]):>10s} {'-'*len(headers[2]):>10s} {'-'*len(headers[3]):>10s}'
+    seps = f'{'-'*10:>10s} {'-'*10:>10s} {'-'*10:>10s} {'-'*10:>10s}'
+    report = make_report(file1, file2)
+    print(hs)
+    print(seps)
+
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
+
+if __name__ == '__main__':
+    main()
